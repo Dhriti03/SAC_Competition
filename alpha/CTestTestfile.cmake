b0ae4f64193191aeaef8363ece9c1fb54707edf6
Dhriti_03 <84375995+Dhriti03@users.noreply.github.com>
2022-06-04 12:12:27 +0530

    uploading alpha & atom folder under build folder

---
+++ alpha/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhriti/sac_ws/src/alpha
+# Build directory: /home/dhriti/sac_ws/build/alpha
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ atom/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhriti/sac_ws/src/atom
+# Build directory: /home/dhriti/sac_ws/build/atom
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
