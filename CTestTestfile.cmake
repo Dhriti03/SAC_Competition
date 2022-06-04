1459c72477569877905ebb3cdf459e06f77edf65
Dhriti_03 <84375995+Dhriti03@users.noreply.github.com>
2022-06-04 12:16:25 +0530

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhriti/sac_ws/src
+# Build directory: /home/dhriti/sac_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("sac_arena/arena_with_qr1")
+subdirs("live_demo")
+subdirs("alpha")
+subdirs("beta_description")
+subdirs("atom")
+subdirs("sac_final_2022")
---
+++ live_demo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhriti/sac_ws/src/live_demo
+# Build directory: /home/dhriti/sac_ws/build/live_demo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
