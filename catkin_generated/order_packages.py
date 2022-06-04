56487c08018a82d628df2d0b7d222084c76253ac
Dhriti_03 <84375995+Dhriti03@users.noreply.github.com>
2022-06-04 12:15:11 +0530

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/dhriti/sac_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/dhriti/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/dhriti/catkin_ws/devel;/opt/ros/noetic' != '' else []
