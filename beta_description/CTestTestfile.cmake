adca071b9b4b082419c12b9cb9380a71018fe2c1
Dhriti_03 <84375995+Dhriti03@users.noreply.github.com>
2022-06-04 12:13:17 +0530

    folder uploads

---
+++ beta_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dhriti/sac_ws/src/beta_description
+# Build directory: /home/dhriti/sac_ws/build/beta_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
